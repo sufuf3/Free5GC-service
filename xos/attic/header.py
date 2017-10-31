1fd2e92364d27df3cb63cddb775630dc526050b7
Scott Baker <smbaker@gmail.com>
2017-10-31 13:37:00 -0700

    CORD-2184: Remove leftover artifacts from exampleservice
    Change-Id: I9f41b2e80c666a6ed5ab57ceb6dfb569e8f82578
    (cherry picked from commit c9cdf759b9a70ce2c06f078813a8100c2ee3eb8c)

--- xos/attic/header.py
+++ xos/attic/header.py
@@ -17,11 +17,4 @@
 # models.py -  ExampleService Models
 
 from core.models import Service, TenantWithContainer, XOSBase
-from django.db import transaction
 from django.db.models import *
-
-SERVICE_NAME = 'exampleservice'
-SERVICE_NAME_VERBOSE = 'Example Service'
-SERVICE_NAME_VERBOSE_PLURAL = 'Example Services'
-SERVICE_INSTANCE_NAME_VERBOSE = 'Example Service Instance'
-SERVICE_INSTANCE_VERBOSE_PLURAL = 'Example Service Instances'
